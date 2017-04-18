bd59da8cbf237794c1a5c2fbc41a60516ff3d691
Pascal Bertschi <pascal.bertschi@hotmail.com>
2017-04-18 17:48:13 +0200

    feat: added migrate api route

--- Backend/Controllers/ValuesController.cs
+++ Backend/Controllers/ValuesController.cs
@@ -3,19 +3,23 @@ using System.Linq;
 using Backend.Hubs;
 using Microsoft.AspNetCore.Mvc;
 using Business.Services;
+using Database;
 using Database.Models;
 using Microsoft.AspNetCore.SignalR.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        private IConnectionManager connectionManager;
+        private readonly IConnectionManager connectionManager;
+        private readonly WaitlessContext context;
 
-        public ValuesController(IConnectionManager connectionManager)
+        public ValuesController(IConnectionManager connectionManager, WaitlessContext context)
         {
             this.connectionManager = connectionManager;
+            this.context = context;
         }
 
         // GET api/values
@@ -24,6 +28,14 @@ namespace Backend.Controllers
         {
             var hubContext = connectionManager.GetHubContext<TabletHub>();
             hubContext.Clients.All.OnAssignedTablet(mode);
+        }
+
+        // GET api/values
+        [HttpGet]
+        [Route("migrate")]
+        public void Migrate()
+        {
+            context.Database.Migrate();
         }
 
         // GET api/values/5
