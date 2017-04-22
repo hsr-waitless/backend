743e7a040c9b0527edbe836a3f8515ce165af31b
lexli8888 <alexander.van.schie@hsr.ch>
2017-04-22 10:27:16 +0200

    Unittest für Assign/Unassign Order

---
+++ Business.Test/UnitTestAssignOrderService.cs
@@ -0,0 +1,106 @@
+﻿using Business.Services;
+using Business.Test.Factory;
+using Database;
+using Database.Models;
+using Microsoft.EntityFrameworkCore;		
+using System;
+using System.Collections.Generic;
+using System.Linq;		
+using Xunit;		
+ 		
+ namespace Backend.Test		
+ {		
+     public class UnitTestAssignOrderService		
+     {
+        [Fact]
+        public void TestOnOrderAssigned()
+        {
+            var context = MockContextFactory.Create();
+
+            var guest = new Tablet()
+            {
+                Id = 1,
+                Identifier = "Hachim",
+                Mode = Mode.Guest
+            };
+
+            var waiter = new Tablet()
+            {
+                Id = 2,
+                Identifier = "Karim",
+                Mode = Mode.Waiter
+            };
+
+            var testOrder = new Order()
+            {
+                Id = 1,
+                OrderStatus = OrderStatus.New,
+                CreationTime = new DateTime(2017, 2, 4, 17, 0, 0),
+                UpdateTime = new DateTime(2017, 2, 4, 17, 0, 0),
+                Waiter = waiter,
+                Guests = new List<Tablet>()
+               
+
+            };
+
+            context.Tablet.Add(guest);
+            context.Tablet.Add(waiter);
+            context.Order.Add(testOrder);
+            context.SaveChanges();
+
+            var service = new AssignOrderService(context);
+            var result = service.OnOrderAssigned(guest.Identifier, testOrder.Id);
+            Assert.Equal(guest.Identifier, testOrder.Guests.FirstOrDefault().Identifier);
+            Assert.Equal(true, result);
+
+        }
+
+        [Fact]
+        public void TestOnOrderUnassigned()
+        {
+            var context = MockContextFactory.Create();
+
+            var guest = new Tablet()
+            {
+                Id = 1,
+                Identifier = "Hachim",
+                Mode = Mode.Guest
+            };
+
+            var waiter = new Tablet()
+            {
+                Id = 2,
+                Identifier = "Karim",
+                Mode = Mode.Waiter
+            };
+
+            var testOrder = new Order()
+            {
+                Id = 1,
+                OrderStatus = OrderStatus.New,
+                CreationTime = new DateTime(2017, 2, 4, 17, 0, 0),
+                UpdateTime = new DateTime(2017, 2, 4, 17, 0, 0),
+                Waiter = waiter,
+                Guests = new List<Tablet>()
+
+
+            };
+
+            context.Tablet.Add(guest);
+            context.Tablet.Add(waiter);
+            context.Order.Add(testOrder);
+            context.SaveChanges();
+
+            var service = new AssignOrderService(context);
+            var result = service.OnOrderAssigned(guest.Identifier, testOrder.Id);
+            result = service.OnOrderUnassigned(guest.Identifier, testOrder.Id);
+            
+            Assert.Equal(true, result);
+            Assert.Empty(testOrder.Guests);
+
+        }
+
+
+
+    }		
+ }
